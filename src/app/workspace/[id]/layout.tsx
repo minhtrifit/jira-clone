9bd85441e1e0b9d86dfa55f8d4ee0a6f4364b2ab
minhtrifit <minhtri.fit@gmail.com>
2025-02-13 17:29:36 +0700

    refactor: navbar layout

--- src/app/workspace/[id]/layout.tsx
+++ src/app/workspace/[id]/layout.tsx
@@ -3,6 +3,7 @@ import { ModeToggle } from "@/components/ModeToggle/ModeToggle";
 import { AppSidebar } from "@/components/Slidebar/AppSlidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import NotificationBtn from "@/components/NotificationBtn/NotificationBtn";
+import AppNavbar from "@/components/AppNavbar/AppNavbar";
 
 export const metadata: Metadata = {
   title: `${process.env.NEXT_PUBLIC_APP_NAME ?? "Jira clone"} | My Workspace`,
@@ -19,15 +20,8 @@ export default function Layout({
       <AppSidebar />
       <main className="w-full relative">
         <div className="w-full p-5 mt-0 mb-[43.19px]">
-          <div className="mb-5 flex items-center justify-between gap-3">
-            <SidebarTrigger />
-
-            <div className="flex items-center gap-3">
-              <NotificationBtn />
-              <ModeToggle />
-            </div>
-          </div>
-          {children}
+          <AppNavbar />
+          <div className="mt-[60px]">{children}</div>
         </div>
         <footer className="bg-zinc-100 dark:bg-slate-900 w-full py-3 absolute bottom-0 flex items-center justify-center">
           <span className="text-[0.8rem]">Developed by minhtrifit with ❤️</span>
